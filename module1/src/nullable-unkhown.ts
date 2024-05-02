3950fe12e594c4f9f0f1e78cac4051a4cd2fbbcc
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2024-05-02 21:19:03 +0600

    nullable-unknown

---
+++ module1/src/nullable-unkhown.ts
@@ -0,0 +1,28 @@
+{
+  //? Nullable
+
+  const searchName = (value: string | null) => {
+    if (value) {
+      console.log("Value is found !!");
+    } else {
+      console.log("No value found @@@");
+    }
+  };
+
+  searchName(null);
+
+  //?unknown ------ type of
+
+  const getSpeedMeterPerSecond = (value: unknown) => {
+    if (typeof value === "number") {
+      const convertedSpeed = (value * 1000) / 3600;
+      console.log(convertedSpeed);
+    } else if (typeof value === "string") {
+      const [result, unit] = value.split(" ");
+      const convertedSpeed = (parseFloat(result) * 1000) / 36000;
+      console.log(convertedSpeed, unit);
+    }
+  };
+
+  getSpeedMeterPerSecond("2000 ms^-1");
+}
